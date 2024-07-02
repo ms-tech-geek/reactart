1d2a340e4224280f3cbe9ac3530cba6aea70afb7
Mayank Sethi <mayank@digibrain.co.in>
2024-07-02 18:03:05 +0530

    style button component with tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { styled } from 'styled-components';
+import { useState } from "react";
+import { styled } from "styled-components";
+import Button from "./Button";
 
 const ControlContainer = styled.div`
   display: flex;
@@ -15,42 +16,29 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? '#f87171' : '#6b7280')};
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? '#fed2d2' : '#d1d5db')};
-  color: ${({ $invalid }) => ($invalid ? '#ef4444' : '#374151')};
-  border: 1px solid ${({ $invalid }) => ($invalid ? '#f73f3f' : 'transparent')};
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
   border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
+  box-shadow:
+    0 1px 3px 0 rgba(0, 0, 0, 0.1),
+    0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -61,7 +49,7 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
@@ -72,7 +60,7 @@ export default function AuthInputs() {
           <Input
             type="email"
             $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
@@ -81,7 +69,7 @@ export default function AuthInputs() {
             type="password"
             $invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>

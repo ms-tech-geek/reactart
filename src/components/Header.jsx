affd6c7529299abd24e03b071b6254fbf5a639b5
Mayank Sethi <mayank@digibrain.co.in>
2024-07-02 17:54:40 +0530

    responsive styling with tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header className="mb-16 mt-8 flex flex-col items-center">
+    <header className="mb-8 mt-8 flex flex-col items-center md:mb-16">
       <img
         src={logo}
         alt="A canvas"
         className="mb-8 h-44 w-44 object-contain"
       />
-      <h1 className="font-title text-center text-4xl font-semibold uppercase tracking-widest text-amber-800">
+      <h1 className="font-title text-center text-xl font-semibold uppercase tracking-widest text-amber-800 md:text-4xl">
         ReactArt
       </h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
